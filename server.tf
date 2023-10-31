b39140c88e2c15114ddd4cd0c9ef997710eddf61
kunjay-ni <145367165+kunjay-ni@users.noreply.github.com>
2023-10-31 14:22:32 -0400

    fix: added load balancer listner rule to forward ecs service's port

--- server.tf
+++ server.tf
@@ -140,6 +140,7 @@ resource "aws_ecs_service" "mlflow" {
 
   depends_on = [
     aws_lb.mlflow,
+    aws_lb_listener.mlflow,
   ]
 }
 
@@ -210,3 +211,13 @@ resource "aws_lb_target_group" "mlflow" {
   }
 }
 
+resource "aws_lb_listener" "mlflow" {
+  load_balancer_arn = aws_lb.mlflow.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.mlflow.arn
+  }
+}
