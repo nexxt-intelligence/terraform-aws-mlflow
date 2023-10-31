926793b8fde4ce2f16a8eceffc59cf04707877b3
kunjay-ni <145367165+kunjay-ni@users.noreply.github.com>
2023-10-31 14:01:43 -0400

    fix: s3 resources & options updated to support latest aws provider version 5.x

--- s3.tf
+++ s3.tf
@@ -1,36 +1,63 @@
 resource "aws_s3_bucket" "default" {
   count         = local.create_dedicated_bucket ? 1 : 0
   bucket_prefix = var.unique_name
-  acl           = "private"
-
-  versioning {
-    enabled = true
-  }
-
-  lifecycle_rule {
-    id      = "history"
-    enabled = true
-    transition {
-      days          = 60
-      storage_class = "INTELLIGENT_TIERING"
-    }
-  }
-
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        kms_master_key_id = var.artifact_bucket_encryption_key_arn
-        sse_algorithm     = var.artifact_bucket_encryption_algorithm
-      }
-    }
-  }
-
+  
   tags = merge(
     {
       Name = "${var.unique_name}-default-artifact-root"
     },
     local.tags
   )
+}
+
+resource "aws_s3_bucket_ownership_controls" "default" {
+  count = local.create_dedicated_bucket ? 1 : 0
+  bucket = aws_s3_bucket.default[0].id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_acl" "default" {
+  count = local.create_dedicated_bucket ? 1 : 0
+  bucket = aws_s3_bucket.default[0].id
+  acl = "private"
+  depends_on = [ aws_s3_bucket.default, aws_s3_bucket_ownership_controls.default ]
+}
+
+resource "aws_s3_bucket_versioning" "default" {
+  count = local.create_dedicated_bucket ? 1 : 0
+  bucket = aws_s3_bucket.default[0].id
+  versioning_configuration {
+    status = "Enabled"
+  }
+  depends_on = [ aws_s3_bucket.default ]
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "default" {
+  count = local.create_dedicated_bucket ? 1 : 0
+  bucket = aws_s3_bucket.default[0].id
+
+  rule {
+    status = "Enabled"
+    id = "history"
+    transition {
+      days          = 60
+      storage_class = "INTELLIGENT_TIERING"
+    }
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "default" {
+  count = local.create_dedicated_bucket ? 1 : 0
+  bucket = aws_s3_bucket.default[0].id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = var.artifact_bucket_encryption_key_arn
+      sse_algorithm     = var.artifact_bucket_encryption_algorithm
+    }
+  }
 }
 
 resource "aws_iam_role_policy" "default_bucket" {
